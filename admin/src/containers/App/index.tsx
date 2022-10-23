3e27bdc146bfcd2993f311dd00ea6d6ceb2e7393
cooper_pr <8416844+nickpartum@users.noreply.github.com>
2022-10-23 10:24:57 +0300

    Added admin

---
+++ admin/src/containers/App/index.tsx
@@ -0,0 +1,15 @@
+import React, {FC} from "react"
+import {} from '@mui/material';
+import {} from "@mui/icons-material";
+
+interface ICProps {
+
+}
+
+const App: FC<ICProps> = (props) => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
---
+++ admin/src/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+    api: {
+        host: 'http://localhost',
+        port: 3003,
+        endPoint: '/api'
+    }
+}
